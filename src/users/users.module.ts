8f7199a61306f1fd6ebaaa6668787f58439e91e2
Georgiana Marian <georgi.marian@yahoo.com>
2023-05-12 19:55:43 +0300

    Change import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from 'src/users/users.controller';
+import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from './users.service';
 
 @Module({
 	controllers: [UsersController],
